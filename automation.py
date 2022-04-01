79a3dbeaecbab1ba4ecdcec1094a3109810f6e87
hashtagd3v <jaycoinminer@gmail.com>
2022-03-31 23:47:47 -0500

    Automated typing an input message on a form, clicking the show message button, then showing the output message.

--- automation.py
+++ automation.py
@@ -6,5 +6,18 @@ chrome_browser.maximize_window()
 chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 
 assert 'Selenium Easy Demo' in chrome_browser.title
-button_text = chrome_browser.find_element_by_class_name('btn-default')
-print(button_text.get_attribute('innerHTML'))
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+print(show_message_button.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
+print(user_button2)
+user_message.clear()
+user_message.send_keys('HELLO WORLD!!!')
+
+show_message_button.click()
+output_message = chrome_browser.find_element_by_id('display')
+
+assert 'HELLO WORLD!!!' in output_message.text
